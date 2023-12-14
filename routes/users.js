f117af8b9e1af493d9bd9b999ec213bbdb65c89a
MerwanDev <mlaakad@yahoo.com>
2023-12-14 15:58:23 +0100

    add signup

--- routes/users.js
+++ routes/users.js
@@ -2,27 +2,41 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
-});
 
-router.post("/", async (req, res) => {
-    try {
+router.post('/signup', (req, res) => {
+  const { username, email, password } = req.body;
 
-        const {username, email, password} = req.body;
-        const querry = "SELECT * FROM users";
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: 'Toutes les informations sont requises.' });
+  }
 
-        if (!username)
-            throw new Error("Il manque le nom d'utilisateur");
+  const sanitizedUsername = username.replace(/[^\w\s]/gi, '');
+  const sanitizedEmail = email.replace(/[^\w\s@.]/gi, '');
 
-        // const responseQuery = await db.query(querry, [username]);
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!passwordRegex.test(password)) {
+    return res.status(400).json({ message: 'Le mot de passe ne respecte pas les consignes de sécurité.' });
+  }
 
-
-    }catch(e) {
-
+  const checkEmailQuery = 'SELECT * FROM users WHERE email = ?';
+  db.query(checkEmailQuery, [sanitizedEmail], (err, result) => {
+    if (err) {
+      throw err;
     }
 
-})
+    if (result.length > 0) {
+      return res.status(400).json({ message: 'Cet email est déjà utilisé.' });
+    }
+
+    const addUserQuery = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+    db.query(addUserQuery, [sanitizedUsername, sanitizedEmail, password], (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      res.status(201).json({ message: 'Utilisateur ajouté avec succès.' });
+    });
+  });
+});
 
 module.exports = router;
