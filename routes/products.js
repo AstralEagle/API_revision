476bd283ededf1648ee411e0c25265792272cb8f
a.dias <a.dias@cyberdian.fr>
2023-12-14 14:25:10 +0100

    add routes products

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+// /products/
+router.get('/', async (req, res) => {
+   try{
+       const query = "SELECT * FROM products";
+       const products = await db.query(query);
+       res.json(products)
+   }
+   catch (e) {
+       res.status(400).json({error: e.message})
+   }
 });
 
+// /products/:id
+router.get('/:id', async (req, res) => {
+    try{
+        const id = parseInt(req.params.id)
+        const query = "SELECT * FROM products WHERE id = ?";
+        const product = await db.query(query, [id]);
+        if(!product.length)
+            throw new Error("Produit introuvable")
+        res.json(product);
+    }
+    catch (e) {
+        res.status(400).json({error: e.message})
+    }
+});
+
+
+// /products/
 router.post("/add", async (req, res) => {
     try {
         const {title, price, description} = req.body;
@@ -15,7 +39,7 @@ router.post("/add", async (req, res) => {
         const query1 = "SELECT * FROM products WHERE title = ?"
         const productWithName = await db.query(query1, [title])
 
-        if(productWithName.length())
+        if(productWithName.length)
             throw new Error("Le produit existe deja")
 
         const query2 = "INSERT INTO products (title, price, description, created_ad) VALUES (?, ?, ?, ?)"
@@ -27,6 +51,24 @@ router.post("/add", async (req, res) => {
     }
     catch(e) {
       res.status(400).json({error: e.message})
+    }
+})
+
+// /products/:id
+router.delete("/:id", async (req, res) => {
+    try {
+        const id = parseInt(req.params.id)
+        const query1 = "SELECT * FROM products WHERE id = ?";
+        const product = await db.query(query1, [id]);
+        if(!product.length)
+            throw new Error("Produit introuvable")
+
+        const query2 = "DELETE FROM products WHERE id = ?"
+        await db.query(query2, [id]);
+        res.json({message: "Produit supprimer"})
+    }
+    catch(e) {
+        res.status(400).json({error: e.message})
     }
 })
 
